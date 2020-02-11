ca9bdd22f70367861b320c27d6d62f4db29abb0d
linjuanmao <53474973+linjuanmao@users.noreply.github.com>
2020-02-11 14:45:46 +0800

    12004.js

---
+++ univ/12004.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "华南师范大学",
+    id: "12004",
+    link: [ {
+        name: "非正式课程",
+        url: "https://fzskc.scnu.edu.cn/opt_jx_wdkc.aspx?key=br1YVLauA11WShKV&fid=156"
+    }, {
+        name: "教学信息系统",
+        url: "https://jwxt.scnu.edu.cn/xtgl/index_initMenu.html?jsdm=&_t=1576449472769"
+    }, {
+        name: "图书馆",
+        url: "http://lib.scnu.edu.cn/"
+    }, {
+        name: "学生服务平台",
+        url: "https://ssp.scnu.edu.cn/"
+    }, {
+        name: "十校互选",
+        url: "https://unitown.scnu.edu.cn/default.aspx"
+    }, {
+        name: "砺儒云课堂",
+        url: "http://moodle.scnu.edu.cn/"
+    }, {
+        name: "正方",
+        url: "https://jwc.scnu.edu.cn/"
+    } ]
+};
